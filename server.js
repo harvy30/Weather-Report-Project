7a8d1cb7a4353949cfc1eab50fd5b690daef975f
iharsimran30 <iharsimran30@gmail.com>
2016-12-22 16:01:49 -0500

    redirecting to https to http

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 // Create our app
 var app = express();
-var port = process.env.PORT \\ 9005;
+var port = process.env.PORT || 9005;
 //env = Environment
+app.use(Function (req, res, next){
+  if(req.header['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url)
+  }
+});
 
 app.use(express.static('public'));
 
